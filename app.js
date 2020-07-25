e81627193884d0d9fbe48dce986481f52923dfbe
Musthafin Be Fahmaziz <67007894+afinmusthafin@users.noreply.github.com>
2020-07-25 22:04:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,127 @@
-//
+// DOM Elements
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+
+const valueEl = document.querySelector('.value');
+
+const cEl = document.querySelector('.c');
+const rbEl = document.querySelector('.rb');
+const percentEl = document.querySelector('.percent');
+
+const divisionEl = document.querySelector('.division');
+const multiplicationEl = document.querySelector('.multiplication');
+const substractionEl = document.querySelector('.substraction');
+const additionEl = document.querySelector('.addition');
+
+const pmEl = document.querySelector('.pm');
+
+const decimalEl = document.querySelector('.decimal');
+const equalEl = document.querySelector('.equal');
+
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const number0El = document.querySelector('.number-0');
+const numberElArray = [
+  number1El, number2El, number3El, number4El, number5El, number6El, number7El, number8El, number9El, number0El
+];
+
+
+// Functions
+const getValueAsStr = () => return valueEl.textContent.split(',').join('');
+
+const getValueAsNum = () => {
+  return parseFloat(getValueAsStr());
+};
+
+const setStrAsValue = (valueStr) => {
+  if (valueStr[valueStr.length - 1] == '.') {
+    valueEl.textContent += '.';
+    return;
+  }
+
+  const [wholeNumStr, decimalStr] = valueStr.split('.');
+  if (decimalStr) {
+    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+  } else {
+    vlueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
+  }
+};
+
+const handleNumberClick = (numStr) => {
+  const currentValueStr = getValueAsStr();
+  if (currentValueStr === '0') {
+    setStrAsValue(numStr);
+  } else {
+    setStrAsValue(currentValueStr + numStr);
+  }
+
+};
+
+
+// Add Event Listeners to Functions
+cEl.addEventListener('click', () => {
+  setStrAsValue('0');
+});
+pmEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const currentValueStr = getValueAsStr();
+
+  if (currentValueStr == '-0') {
+    setStrAsValue('0');
+    return;
+  }
+  if (currentValueNum >= 0) {
+    setStrAsValue('-' + currentValueStr.substring(1));
+  } else {
+    setStrAsValue(currentValueStr)
+  }
+});
+percentEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const newValueNum = currentValueNum / 100;
+  setStrAsValue(newValueNum.toString());
+});
+
+
+// Add Event Listeners to numbers and decimal
+for (let i = 0; i < numberElArray.length; i++) {
+  const numberEl = numberElArray[i];
+  numberEl.addEventListener('click', () => {
+    handleNumberClick(i.toString());
+  });
+};
+decimalEl.addEventListener('click', () => {
+  const currentValueStr = getValueAsStr();
+  if (!currentValueStr.includes('.')) {
+    setStrAsValue(currentValueStr + '.');
+  }
+});
+
+
+
+
+
+// Set up the time
+const updateTime = () => {
+  const currentTime = new Date();
+
+  let currentHour = currentTime.getHours();
+  const currentMinute = currentMinute.getMinutes();
+
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+
+  hourEl.textContent = currentHour.toString();
+  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+};
+
+setInterval(updateTime, 1000);
+updateTime();
